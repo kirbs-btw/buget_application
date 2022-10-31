6014d6e4b64f0716c019b8c1a8c23e7cbdc12c31
Bastian Lipka <68615019+kirbs-btw@users.noreply.github.com>
2022-10-31 11:02:17 +0100

    Add files via upload

--- main.dart
+++ main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
 
 void main() => runApp(MyApp());
 
@@ -9,15 +10,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
+
         primarySwatch: Colors.blueGrey,
       ),
       home: MyHomePage(title: 'Flutter Demo Home Page'),
@@ -28,15 +21,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -44,23 +28,88 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _money = 0;
+  double money = 0;
   List<Widget> _newEntry = [];
-  void _addEntry() {
-    setState(() {
-      _newEntry.add(_newContainer());
 
+  double amount = 19.00;
+  String description = "fuck";
+
+  // format double amount
+  // add a 0 if missing to shot money
+  // format function
+  void updateDescription(String text){
+    description = text;
+  }
+
+  void updateAmount(String text){
+    amount = double.parse(text);
+  }
+
+  _addEntryCard(BuildContext context){
+    return showDialog(context: context, builder: (context){
+      return AlertDialog(
+        title: Text("new entry"),
+        content: SizedBox(
+          height: 100,
+          child: Column(
+
+            children: <Widget>[
+              TextField(
+                onChanged: updateDescription,
+                decoration: InputDecoration(
+                  hintText: "description",
+                ),
+              ),
+              TextField(
+                onChanged: updateAmount,
+                decoration: InputDecoration(
+                  hintText: "00.00",
+                ),
+              ),
+            ],
+          ),
+        ),
+        actions: <Widget>[
+          MaterialButton(
+            elevation: 5.0,
+            child: Text("add"),
+            onPressed: (){
+              addEntry(context);
+            },
+          ),
+          MaterialButton(
+            elevation: 5.0,
+            child: Text("cancel"),
+            onPressed: (){
+              Navigator.of(context).pop();
+            },
+          ),
+        ],
+      );
     });
   }
+
+  void addEntry(context) {
+    Navigator.of(context).pop();
+    money = money + amount;
+    print(money);
+    money = double.parse(money.toStringAsFixed(2));
+    print(money);
+
+    setState(() {
+      _newEntry.add(_newContainer());
+    });
+  }
+
   Widget _newContainer() {
-    return Dismissible(
-      child: Container(
-        child: ListTile(
-          title: Text("hello"),
-          subtitle: Text("hello"),
-          leading: Icon(Icons.dashboard),
-        ),
+    return Container(
+      width: 100,
+      child: ListTile(
+        title: Text('$amount€'),
+        subtitle: Text(description),
+        leading: Icon(Icons.attach_money),
       ),
+      color: amount < 0 ? Colors.deepOrangeAccent : Colors.lightGreen,
     );
   }
 
@@ -71,7 +120,7 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         centerTitle: true,
         title: Text(
-          '$_money€',
+          '$money€',
           style: TextStyle(
             fontFamily: 'Gothamhft',
             // insert font to files -------------------------------------
@@ -83,15 +132,16 @@ class _MyHomePageState extends State<MyHomePage> {
       body: ListView.builder(
         itemCount: _newEntry.length,
         itemBuilder: (context, index){
-        return _newEntry[index];
+          return _newEntry[index];
         },
       ),
 
       floatingActionButton: FloatingActionButton(
-        // change the function to an add widget button
-        onPressed: _addEntry,
+        onPressed: (){
+          _addEntryCard(context);
+        },
         tooltip: 'Increment',
-        child: Icon(Icons.add),
+        child: Icon( Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
